f3d47757e990b23223f75ddb4d9ecfadc4c80b1b
Andrey Reshetov <andr.reshetov@gmail.com>
2025-11-19 15:54:10 +0300

    Added ENV for puma workers, fixed exceptions

--- config/puma.rb
+++ config/puma.rb
@@ -25,8 +25,10 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
-threads threads_count, (threads_count * 2)
+threads_count = ENV.fetch("RAILS_PUMA_THREADS", 3)
+threads threads_count, (threads_count.to_i * 2)
+
+workers ENV.fetch("RAILS_PUMA_WORKERS", Rails.env.development? ? 0 : 2)
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
